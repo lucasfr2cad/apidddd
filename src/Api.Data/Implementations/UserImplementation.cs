4d6dc4630818a8f804d03bb584e4af6db7c2fa57
antoni-sganzerla-reitech <antoni@glasscad.com.br>
2019-10-28 15:46:20 -0300

    alterado visibilidade _dataset BaseRepository

--- src/Api.Data/Implementations/UserImplementation.cs
+++ src/Api.Data/Implementations/UserImplementation.cs
@@ -9,11 +9,10 @@ namespace Api.Data.Implementations
 {
     public class UserImplementation : BaseRepository<UserEntity>, IUserRepository
     {
-        private DbSet<UserEntity> _dataset;
 
         public UserImplementation(MyContext context) : base(context)
         {
-            _dataset = context.Set<UserEntity> ();
+            
         }
 
         public async Task<UserEntity> FindByLogin(string ds_nome)
