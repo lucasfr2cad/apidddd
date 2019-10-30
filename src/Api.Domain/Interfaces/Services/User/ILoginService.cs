db1886a5985b1fc4e488c7d314c516f0409318c7
antoni-sganzerla-reitech <antoni@glasscad.com.br>
2019-10-30 10:27:42 -0300

    Criado classe LoginResponse

--- src/Api.Domain/Interfaces/Services/User/ILoginService.cs
+++ src/Api.Domain/Interfaces/Services/User/ILoginService.cs
@@ -1,17 +1,18 @@
 using System.Threading.Tasks;
 using Api.Domain.Dtos;
 using Api.Domain.Entities;
+using Api.Domain.Utils;
 
 namespace Api.Domain.Interfaces.Services.User
 {
     public interface ILoginService
     {
-          Task<object>  FindByLogin(LoginDto user);
+        Task<LoginResponse> FindByLogin(LoginDto user);
 
-          Task<UserEntity> Post (UserEntity user);
+        Task<UserEntity> Post(UserEntity user);
 
 
-          Task<SessionEntity> UnchekSession (int id);
+        Task<SessionEntity> UnchekSession(int id);
 
     }
 }
