5ce1357f5dfd00980a63925ae4151d5da66172ff
Antoni <antoni@glasscad.com.br>
2019-11-14 17:26:00 -0300

    focaga provides

--- src/Api.Application/Controllers/ClientsController.cs
+++ src/Api.Application/Controllers/ClientsController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.Client;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Application.Controllers
@@ -17,6 +19,8 @@ namespace Api.Application.Controllers
             _service = service;
         }
 
+        //[Authorize(Policy = "actionRequirement")]
+        [Authorize("Bearer")]
         [HttpGet]
         public async Task<ActionResult> GetAll()
         {
@@ -32,6 +36,80 @@ namespace Api.Application.Controllers
             {
                 return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
             }
+        }
+
+        [Authorize("Bearer")]
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ClientEntity client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); //400 
+            }
+            try
+            {
+                var result = await _service.Post(client);
+                if (result != null)
+                {
+                    // caso criado retorna o objeto criado e no cabeçalho uma url para a consulta
+                    return Created(new Uri(Url.Link("GetWhithId", new { id = result.cd_codigo })), result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] ClientEntity client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); //400 
+            }
+            try
+            {
+                var result = await _service.Put(client);
+
+                if (result != null)
+                {
+                    // caso criado retorna o objeto criado e no cabeçalho uma url para a consulta
+                    return Ok(result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); //400 
+            }
+            try
+            {
+                return Ok(await _service.Delete(id));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+
         }
     }
 }
