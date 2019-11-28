135e274be7668e3a976a150de07d510d76b8e195
Antoni <antoni@glasscad.com.br>
2019-11-28 10:15:42 -0300

    datasource via construtor

--- src/Api.Application/Data/MyData.cs
+++ src/Api.Application/Data/MyData.cs
@@ -3,8 +3,17 @@ using System.Data;
 
 namespace Api.Application.Data
 {
-    public class MyData
+    public class MyData: DataTable
     {
+
+        public MyData(){
+            DataTable dt = new DataTable();
+            dt.Columns.Add(new DataColumn("ID", typeof(Int32)));
+            dt.Columns.Add(new DataColumn("Name", typeof(String)));
+            dt.Rows.Add(1, "Lucas");
+            dt.Rows.Add(2, "Fogaça");
+            this.Merge(dt);
+        }
 
         public static DataTable GetData()
         {
